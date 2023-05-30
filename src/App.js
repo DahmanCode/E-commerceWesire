befeb03e47a02791415ea101c7410573f9adafd2
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-05-30 01:43:15 +0100

    start on a new project 'Ecommerce Website'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./compononents/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
