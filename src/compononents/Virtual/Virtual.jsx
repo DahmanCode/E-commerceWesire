2535c6f1819e1350690cdd1dba286c6b54a4b718
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-06-01 21:40:22 +0100

    component

--- src/compononents/Virtual/Virtual.jsx
+++ src/compononents/Virtual/Virtual.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import css from './Virtual.module.css'
 import Shade from '../../assets/shade.png'
+import ReactCompareImage from 'react-compare-image'
+import Before from '../../assets/before.png'
+import After from '../../assets/after.png'
 
 function Virtual() {
   return (
     <div className={css.Virtual}>
+
       <div className={css.left}>
         <span>Virtual Try-On</span>
         <span>Never Buy the wrong Shade Again!</span>
@@ -13,7 +17,12 @@ function Virtual() {
       </div>
 
       <div className={css.right}>
-        Virtual component
+        <div className={css.wrapper}>
+          <ReactCompareImage
+          leftImage={Before}
+          rightImage={After}
+          />
+        </div>
       </div>
 
     </div>
