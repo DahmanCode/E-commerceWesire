63122a963d0061e98b80432f2e8a4e194f30da67
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-06-01 19:46:35 +0100

    add tow component  &

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Hero from "./compononents/Hero/Hero";
+import Slider from "./compononents/Slider/Slider";
 import Header from "./compononents/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
+      <Slider />
     </div>
   );
 }
